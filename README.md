accc90b69eab21dbe8d922209dacccdfe66c0337
Ashrith <ashrith@me.com>
2020-02-13 11:41:25 -0800

    Add Serverless tempalte

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # aws-nuke-lambda
+
+## Install Serverless Framework for Deployment
+
+> NOTE: The native build of the Rust Lambda requires Docker to be running in the background.
+
+```
+sudo npm install --global npx
+sudo npm install --global serverless
+npm install --save-dev serverless-rust
+```
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use lambda_runtime::{error::HandlerError, lambda, Context};
+use serde_json::Value;
+
 fn main() {
-    println!("Hello, world!");
+    lambda!(handler)
+}
+
+fn handler(event: Value, _: Context) -> Result<Value, HandlerError> {
+    Ok(event)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use serde_json::json;
+
+    #[test]
+    fn handler_handles() {
+        let event = json!({
+            "answer": 42
+        });
+
+        assert_eq!(
+            handler(event.clone(), Context::default()).expect("expected Ok(_) value"),
+            event
+        )
+    }
 }
